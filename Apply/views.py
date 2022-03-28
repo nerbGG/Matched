267f91af0aaf49f52a9496f5f03820256fee30d6
Shujaullah Ahsan <35875311+shujaullah@users.noreply.github.com>
2022-03-28 13:40:48 -0400

    Login page

---
+++ Apply/views.py
@@ -0,0 +1,5 @@
+from django.shortcuts import render, redirect
+from django.contrib.auth import  authenticate, login, logout
+from django.contrib import messages
+
+
--- Matched/settings.py
+++ Matched/settings.py
@@ -37,7 +37,10 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'Apply',
+    "crispy_forms",
 ]
+CRISPY_TEMPLATE_PACK = "bootstrap4"
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
@@ -122,3 +125,5 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+LOGIN_REDIRECT_URL = "/home"
+LOGOUT_REDIRECT_URL = "/home"
