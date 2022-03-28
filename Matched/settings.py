8153ab2df3f031e185e5e9b4f93cd949ebfc7bb3
NERB <60553959+nerbGG@users.noreply.github.com>
2022-03-28 14:10:10 -0400

    home page fix

--- Matched/settings.py
+++ Matched/settings.py
@@ -125,5 +125,5 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
-LOGIN_REDIRECT_URL = "/home"
-LOGOUT_REDIRECT_URL = "/home"
+LOGIN_REDIRECT_URL = "/"
+LOGOUT_REDIRECT_URL = "/"
