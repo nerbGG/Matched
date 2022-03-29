235aad058f278802328f9f8d8d7d51acec014df3
NERB <60553959+nerbGG@users.noreply.github.com>
2022-03-29 16:57:46 -0400

    profile model

--- Apply/models.py
+++ Apply/models.py
@@ -1,3 +1,27 @@
 from django.db import models
+from django.contrib.auth.models import User
+from .validators import validate_file_extension
+
 
 # Create your models here.
+
+
+class Profile(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    birth_date = models.DateField()
+    # edu_choices = [('hs', 'Highschool'), ('ud', 'Undergraduate'), ('gd', 'Graduate')]
+    education = models.CharField(max_length=100)
+    interests = models.CharField(max_length=100, blank=True)
+    sport = models.CharField(max_length=100)
+    # pdf only
+    resume = models.FileField(blank=True, upload_to='uploads/%Y/%m/%d/', validators=[validate_file_extension])
+    profile_pic = models.ImageField(blank=True)
+    success_story = models.TextField(blank=True)
+
+    # location=
+    class Meta:
+        verbose_name = "user profile"
+        verbose_name_plural = "user profile"
+
+    def __unicode__(self):
+        return self.user.username
--- Matched/urls.py
+++ Matched/urls.py
@@ -22,5 +22,5 @@ from django.views.generic.base import TemplateView
 urlpatterns = [
     path('admin/', admin.site.urls),
     path("accounts/", include("django.contrib.auth.urls")),
-    path("", TemplateView.as_view(template_name="../Templates/home.html"), name="home"),
+    path("", TemplateView.as_view(template_name="home.html"), name="home"),
 ]
