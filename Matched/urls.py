9b034f6f1851b958b03c6418d39386ab78d0b3e2
Shujaullah Ahsan <35875311+shujaullah@users.noreply.github.com>
2022-04-26 23:52:15 -0400

    password reset is working

--- Matched/urls.py
+++ Matched/urls.py
@@ -34,5 +34,8 @@ urlpatterns = [
     path("success-stories/", apply_views.all_success_stories, name="allTheUsersSuccessStories"),
     path("success-stories/<selected_filter>", apply_views.filtered_success_stories),
     path("jobs/<selected_filter>/", apply_views.filtered_jobs),
+    path('reset_password/', auth_views.PasswordResetView.as_view(template_name='password_reset.html'), name='reset_password'),
+    path('reset_password_sent/', auth_views.PasswordResetDoneView.as_view(template_name='password_reset_content.html'), name ='password_reset_done'),
+    path('reset_password_complete', auth_views.PasswordResetCompleteView.as_view(template_name='password_reset_complete.html'), name='password_reset_complete'),
 ]
 # urlpatterns += staticfiles_urlpatterns()
