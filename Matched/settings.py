448a9728118cdce9aea8f4daabc360bbe2bad8d6
nerbgg <nrbarthelemy@gmail.com>
2022-05-14 16:42:44 -0400

    trying to fix templates isue that is causing a 500

--- Matched/settings.py
+++ Matched/settings.py
@@ -61,7 +61,7 @@ ROOT_URLCONF = 'Matched.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [BASE_DIR / 'templates']
+        'DIRS': [BASE_DIR / 'Templates']
         ,
         'APP_DIRS': True,
         'OPTIONS': {
